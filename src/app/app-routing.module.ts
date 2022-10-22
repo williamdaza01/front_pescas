f4fa4f07e1dab695ebf500fccee37b8294ed5dc9
Daza <williamdaza04@gmail.com>
2022-10-22 09:40:00 -0500

    Se eliminan las dependencias de los archivos eliminados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,19 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { RepuestosComponent } from './repuestos/repuestos.component'
-import { ClientesComponent } from './clientes/clientes.component'
 import { ModelosComponent } from './modelos/modelos.component'
-import { EmpleadosComponent } from './empleados/empleados.component'
 import { SedesComponent } from './sedes/sedes.component'
-import { MarcasComponent } from './marcas/marcas.component';
 
 const routes: Routes = [
   {path: 'repuestos', component:RepuestosComponent},
   {path: 'sedes', component:SedesComponent},
-  {path: 'empleados', component:EmpleadosComponent},
   {path: 'modelos', component:ModelosComponent},
-  {path: 'clientes', component:ClientesComponent},
-  {path: 'marcas', component:MarcasComponent},
 ];
 
 @NgModule({
